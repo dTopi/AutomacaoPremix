01cb4b062eba9f447e80743b1542cc3d889402bc
dhaluc <enumael@gmail.com>
2019-01-30 16:44:52 -0200

    novaClasse

---
+++ premix/src/main/java/br/com/automacao/premix/test/TestAvds.java
@@ -0,0 +1,14 @@
+package br.com.automacao.premix.test;
+
+import org.junit.Test;
+
+import br.com.automacao.premix.core;
+
+public class TestAvds extends BaseTest{
+ 
+	@Test
+	public void primeiroTesteAvds(){
+		
+	}
+
+}
