080c7c433960c429d694eefb48914b6e1f1a49eb
dhaluc <enumael@gmail.com>
2019-03-28 15:27:03 -0300

    Excluida itens da classe ListLeads

--- premix/src/main/java/br/com/automacao/premix/Leads/test/ListaLeads.java
+++ premix/src/main/java/br/com/automacao/premix/Leads/test/ListaLeads.java
@@ -14,15 +14,6 @@ public class ListaLeads extends BaseTest{
 	private PesquisarPage pesquisar = new PesquisarPage();
 	private LeadsPage leads = new LeadsPage();
 	
-	@Test
-	public void validaListaLead(){
-		statusReport("Leads", "Validação dados lista incial de leads");
-		inicio.clickBtnDrawer();
-		drawer.clickLeads();
-		leads.validaNome("Nome");
-		leads.validaEmpresa("Empresa");
-		leads.validaCelular("Celular");
-		leads.validaStatus("Status");
-		leads.validaTipoDeRegistro("Tipo de Registro");
-	}
+	
+
 }
