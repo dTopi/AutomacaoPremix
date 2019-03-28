c6f6dd5233c7f555700568428354b34064956b5b
dhaluc <enumael@gmail.com>
2019-03-28 18:29:26 -0300

    classe novas criadas

---
+++ premix/src/main/java/br/com/automacao/premix/Leads/test/ValidacaoCampos.java
@@ -0,0 +1,20 @@
+package br.com.automacao.premix.Leads.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
+import junitparams.mappers.CsvWithHeaderMapper;
+
+
+@RunWith(JUnitParamsRunner.class)
+public class ValidacaoCampos {
+	@Test
+	public void validacaoCampos(){
+		
+	}
+	
+	
+
+}
