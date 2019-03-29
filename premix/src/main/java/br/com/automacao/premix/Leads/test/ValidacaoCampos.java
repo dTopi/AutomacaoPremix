9092e005d771833de5589fcc973daf127cf43a28
USER <USER@DESKTOP-1SBTR3E>
2019-03-29 12:22:06 -0300

    Inicio criação cenario

--- premix/src/main/java/br/com/automacao/premix/Leads/test/ValidacaoCampos.java
+++ premix/src/main/java/br/com/automacao/premix/Leads/test/ValidacaoCampos.java
@@ -3,16 +3,24 @@ package br.com.automacao.premix.Leads.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import br.com.automacao.premix.core.BaseTest;
+import br.com.automacao.premix.page.DrawerPage;
+import br.com.automacao.premix.page.InicioPage;
 import junitparams.FileParameters;
 import junitparams.JUnitParamsRunner;
 import junitparams.mappers.CsvWithHeaderMapper;
 
-
 @RunWith(JUnitParamsRunner.class)
-public class ValidacaoCampos {
+public class ValidacaoCampos extends BaseTest {
+	private InicioPage inicio = new InicioPage();
+	private DrawerPage drawer = new DrawerPage();
 	@Test
 	public void validacaoCampos(){
+		statusReport("Validação dos campos","ValidarCampos");
+		inicio.clickBtnDrawer();
+		drawer.clickLeads();
 		
+	
 	}
 	
 	
